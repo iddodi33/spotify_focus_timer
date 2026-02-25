62a89dc6005d3f709d7f757d621b22fecd7cdf69
iddodi33 <63195082+iddodi33@users.noreply.github.com>
2026-02-25 10:21:02 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'focus-timer-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './spotify_focus_timer.html',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
